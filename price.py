ee003b6dd62ece292b856e0d1b3905e02b81d4a0
Howard <suyihao1999@gmail.com>
2022-05-14 15:30:45 +0800

    first refactor

--- price.py
+++ price.py
@@ -1,18 +1,21 @@
 from collections import Counter
 
-def price(books: list)-> float:
-    result = Counter(books)
-
-    n_diff = len(result)
-    discount = 1.0
+def discount(n_diff):
+    percentage = 1.0
 
     if   n_diff == 2:
-        discount = 0.95
+        percentage = 0.95
     elif n_diff == 3:
-        discount = 0.90
+        percentage = 0.90
     elif n_diff == 4:
-        discount = 0.80
+        percentage = 0.80
     elif n_diff == 5:
-        discount = 0.75
+        percentage = 0.75
+        
+    return percentage
+
+def price(books: list)-> float:
+    result = Counter(books)
+    n_diff = len(result)
     
-    return discount * len(books) * 8
+    return discount(n_diff) * len(books) * 8
